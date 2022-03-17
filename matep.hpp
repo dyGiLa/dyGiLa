8a8329fcaf223495e1e459ba4f45c6784b609181
timohyva <hyvarinentimo00@gmail.com>
2022-03-17 14:42:49 +0200

    add type alias for matep class

--- matep.hpp
+++ matep.hpp
@@ -16,45 +16,47 @@
 #include <cmath>
 
 
+using real_t = float;
+
 class MATEP {
 public:
         MATEP() = default; // constructor
 
         // interfaces of dimensional qualities
 	
-        long double Tcp(long double p);
-        long double mEffp(long double p);
-        long double vFp(long double p);
-        long double xi0p(long double p);
-        long double N0p(long double p);
+        real_t Tcp(real_t p);
+        real_t mEffp(real_t p);
+        real_t vFp(real_t p);
+        real_t xi0p(real_t p);
+        real_t N0p(real_t p);
   
 
         // interfaces of dimensionless coeficients; SC-correction parts:
   
-        long double alpha_bar(long double p, long double T);
-        long double beta1_bar(long double p, long double T);
-        long double beta2_bar(long double p, long double T);
-        long double beta3_bar(long double p, long double T);
-        long double beta4_bar(long double p, long double T);
-        long double beta5_bar(long double p, long double T);
+        real_t alpha_bar(real_t p, real_t T);
+        real_t beta1_bar(real_t p, real_t T);
+        real_t beta2_bar(real_t p, real_t T);
+        real_t beta3_bar(real_t p, real_t T);
+        real_t beta4_bar(real_t p, real_t T);
+        real_t beta5_bar(real_t p, real_t T);
         
 private:
 
         // SC-data sheets arries, All associate to SCCO class
-        static const long double c1_arr[18]; 
-        static const long double c2_arr[18];
-        static const long double c3_arr[18];
-        static const long double c4_arr[18];
-        static const long double c5_arr[18];
+        static const real_t c1_arr[18]; 
+        static const real_t c2_arr[18];
+        static const real_t c3_arr[18];
+        static const real_t c4_arr[18];
+        static const real_t c5_arr[18];
         // ***************************************************
         // '''
-        static const long double Tc_arr[18];
-        static const long double Ms_arr[18];
-        static const long double VF_arr[18];
-        static const long double XI0_arr[18];
+        static const real_t Tc_arr[18];
+        static const real_t Ms_arr[18];
+        static const real_t VF_arr[18];
+        static const real_t XI0_arr[18];
 
         // linear interpolation member:
-        long double lininterp(const long double *cX_arr, long double p);
+        real_t lininterp(const real_t *cX_arr, real_t p);
 };
 
 #endif
