93b9b646c40e49f677efa03fcfb5c9733120e041
Kari Rummukainen <kari.rummukainen@helsinki.fi>
2023-12-18 13:34:34 +0200

    New "big" reduction, now compiles on cuda

--- he3sim.cpp
+++ he3sim.cpp
@@ -431,25 +431,39 @@ void scaling_sim::write_moduli() {
     }
 }
 
+
+// Define a convenience enum for addressing the reductions - could use numbers too
+enum {
+      i_suma, i_sumb2, i_sumb3, i_sumb4, i_sumb5, i_suma_we, i_sumb2_we, i_sumb3_we, i_sumb4_we, i_sumb5_we,
+      i_sumk1, i_sumk2, i_sumk3, i_sumk1_we, i_sumk2_we, i_sumk3_we, i_sumb1_we, i_sumkin, i_sumkin_we, 
+      N_REDUCTION 
+};
+
+
 void scaling_sim::write_energies() {
 
   Matep MP;
   
-  Complex<double> suma(0),sumb2(0),sumb3(0),sumb4(0),sumb5(0);
-  Complex<double> suma_we(0),sumb2_we(0),sumb3_we(0),sumb4_we(0),sumb5_we(0);
-  Complex<double> sumk1(0),sumk2(0),sumk3(0);
-  Complex<double> sumk1_we(0),sumk2_we(0),sumk3_we(0);
+
+  // Complex<double> suma(0),sumb2(0),sumb3(0),sumb4(0),sumb5(0);
+  // Complex<double> suma_we(0),sumb2_we(0),sumb3_we(0),sumb4_we(0),sumb5_we(0);
+  // Complex<double> sumk1(0),sumk2(0),sumk3(0);
+  // Complex<double> sumk1_we(0),sumk2_we(0),sumk3_we(0);
   real_t sumb1 = 0;
-  Complex<double> sumb1_we = 0;
-  Complex<double> sumkin(0);
-  Complex<double> sumkin_we(0);
+  // Complex<double> sumb1_we = 0;
+  // Complex<double> sumkin(0);
+  // Complex<double> sumkin_we(0);
   real_t Tp[2];
 
   update_Tp(t, Tp);
 
   real_t ebfe=fmin(MP.f_A_td(Tp[1], Tp[0]),MP.f_B_td(Tp[1], Tp[0]));
-  
-    hila::set_allreduce(false);
+
+    ReductionVector<Complex<double>> red(N_REDUCTION);
+    red = 0;
+    red.allreduce(false);
+
+    // hila::set_allreduce(false);
     onsites(ALL) {
 
       Complex<double> a(0),b2(0),b3(0),b4(0),b5(0);
@@ -484,59 +498,81 @@ void scaling_sim::write_energies() {
 	      // k3 += (A[X + k].column(j) - A[X - k].column(j)).e(al) * (A[X + j].conj().column(k) - A[X - j].conj().column(k)).e(al)/(4.0*config.dx*config.dx);
       }
 
-      sumkin += kin;
-      sumkin_we += bfe * kin;
+      red[i_sumkin] += kin;
+      red[i_sumkin_we] += bfe * kin;
 
-      sumk1 += k1;
-      sumk1_we += bfe * k1;
+      red[i_sumk1] += k1;
+      red[i_sumk1_we] += bfe * k1;
 
-      sumk2 += k2;
-      sumk2_we += bfe *	k2;
+      red[i_sumk2] += k2;
+      red[i_sumk2_we] += bfe *	k2;
 
-      sumk3 += k3;
-      sumk3_we += bfe *	k3;
+      red[i_sumk3] += k3;
+      red[i_sumk3_we] += bfe *	k3;
       
-      suma += a;
-      suma_we += bfe * a;
+      red[i_suma] += a;
+      red[i_suma_we] += bfe * a;
 
       sumb1 += b1;
-      sumb1_we += bfe * b1;
+      red[i_sumb1_we] += bfe * b1;
 
-      sumb2 += b2;
-      sumb2_we += bfe * b2;
+      red[i_sumb2] += b2;
+      red[i_sumb2_we] += bfe * b2;
 
-      sumb3 += b3;
-      sumb3_we += bfe * b3;
+      red[i_sumb3] += b3;
+      red[i_sumb3_we] += bfe * b3;
 
-      sumb4 += b4;
-      sumb4_we += bfe * b4;
+      red[i_sumb4] += b4;
+      red[i_sumb4_we] += bfe * b4;
 
-      sumb5 += b5;
-      sumb5_we += bfe * b5;
+      red[i_sumb5] += b5;
+      red[i_sumb5_we] += bfe * b5;
     }
 
       if (hila::myrank() == 0) {
         double vol = lattice.volume();
-        config.stream << sumkin.re / vol << " " << sumkin.im / vol << " "
-	              << sumkin_we.re / vol << " " << sumkin_we.im / vol << " "
-		      << sumk1.re / vol << " " << sumk1.im / vol << " "
-	              << sumk1_we.re / vol << " " << sumk1_we.im / vol << " "
-		      << sumk2.re / vol	<< " " << sumk2.im / vol << " "
-	              << sumk2_we.re / vol << " " << sumk2_we.im / vol << " "
-		      << sumk3.re / vol	<< " " << sumk3.im / vol << " "
-	              << sumk3_we.re / vol << " " << sumk3_we.im / vol << " "
-		      << suma.re / vol << " " << suma.im / vol << " "
-	              << suma_we.re / vol << " " << suma_we.im / vol << " "
-		      << sumb1 / vol << " "
-	              << sumb1_we.re / vol << " " << sumb1_we.im / vol << " "
-		      << sumb2.re / vol << " " << sumb2.im / vol << " "
-	              << sumb2_we.re / vol << " " << sumb2_we.im / vol << " "
-		      << sumb3.re / vol << " " << sumb3.im / vol << " "
-	              << sumb3_we.re / vol << " " << sumb3_we.im / vol << " "
-		      << sumb4.re / vol << " " << sumb4.im / vol << " "
-	              << sumb4_we.re / vol << " " << sumb4_we.im / vol << " "
-		      << sumb5.re / vol << " " << sumb5.im / vol << " "
-	              << sumb5_we.re / vol << " " << sumb5_we.im / vol << " ";
+        config.stream 
+            << red[i_sumkin] / vol << " "
+	          << red[i_sumkin_we] / vol << " "
+		        << red[i_sumk1] / vol << " "
+	          << red[i_sumk1_we] / vol << " "
+            << red[i_sumk2] / vol	<< " "
+            << red[i_sumk2_we] / vol << " "
+		        << red[i_sumk3] / vol	<< " "
+	          << red[i_sumk3_we] / vol << " "
+		        << red[i_suma] / vol << " "
+	          << red[i_suma_we] / vol << " "
+		        << sumb1 / vol << " "
+	          << red[i_sumb1_we] / vol << " "
+		        << red[i_sumb2] / vol << " "
+	          << red[i_sumb2_we] / vol << " "
+		        << red[i_sumb3] / vol << " " 
+	          << red[i_sumb3_we] / vol << " "
+		        << red[i_sumb4] / vol << " "
+	          << red[i_sumb4_we] / vol << " "
+		        << red[i_sumb5] / vol << " "
+	          << red[i_sumb5_we] / vol << " ";
+
+        // config.stream << sumkin.re / vol << " " << sumkin.im / vol << " "
+	      //         << sumkin_we.re / vol << " " << sumkin_we.im / vol << " "
+		    //   << sumk1.re / vol << " " << sumk1.im / vol << " "
+	      //         << sumk1_we.re / vol << " " << sumk1_we.im / vol << " "
+		    //   << sumk2.re / vol	<< " " << sumk2.im / vol << " "
+	      //         << sumk2_we.re / vol << " " << sumk2_we.im / vol << " "
+		    //   << sumk3.re / vol	<< " " << sumk3.im / vol << " "
+	      //         << sumk3_we.re / vol << " " << sumk3_we.im / vol << " "
+		    //   << suma.re / vol << " " << suma.im / vol << " "
+	      //         << suma_we.re / vol << " " << suma_we.im / vol << " "
+		    //   << sumb1 / vol << " "
+	      //         << sumb1_we.re / vol << " " << sumb1_we.im / vol << " "
+		    //   << sumb2.re / vol << " " << sumb2.im / vol << " "
+	      //         << sumb2_we.re / vol << " " << sumb2_we.im / vol << " "
+		    //   << sumb3.re / vol << " " << sumb3.im / vol << " "
+	      //         << sumb3_we.re / vol << " " << sumb3_we.im / vol << " "
+		    //   << sumb4.re / vol << " " << sumb4.im / vol << " "
+	      //         << sumb4_we.re / vol << " " << sumb4_we.im / vol << " "
+		    //   << sumb5.re / vol << " " << sumb5.im / vol << " "
+	      //         << sumb5_we.re / vol << " " << sumb5_we.im / vol << " ";
     }
 
        hila::out0 << "Energy done \n";
