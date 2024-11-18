f802b922d2318b69305a20baaee727f67c3cbec5
timohyva <hyvarinentimo00@gmail.com>
2024-11-18 03:43:46 +0200

    add temerature dependent gamma_td block for next() call

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#define USE_PARIO 
+//#define USE_PARIO 
 #define USE_MPI 
 #include <sstream>
 #include <iostream>
@@ -46,7 +46,8 @@ int main(int argc, char **argv) {
     // if (steps == 0)
     //     steps = 1;
 
-    gl.config.gamma              = gl.config.gamma1;                 // initial gamma parameter
+    if (gl.config.TDependnetgamma == 0) { gl.config.gamma = gl.config.gamma1; } // initial gamma parameter if gamma is fixed
+    
     gl.config.boundaryConditions = gl.config.BCs1;                   // initial bounaryConstions
     
     if (gl.config.positions == 1) {
@@ -98,7 +99,7 @@ int main(int argc, char **argv) {
     while (gl.t < gl.config.tEnd) {
       //gl.config.gamma = (stat_counter < gl.config.gammaoffc) ? gl.config.gamma1 : gl.config.gamma2;
       //hila::out0 << "gl.config.gamma is " << gl.config.gamma << "\n" << std::endl;
-        
+      
         if (gl.t >= gl.config.tStats) {
 	  
 	   if (stat_counter % steps == 0) {
@@ -109,17 +110,49 @@ int main(int argc, char **argv) {
 	      //gl.write_phases();
 
 #if defined USE_PARIO
-	      if (gl.t >= gl.config.hdf5Ststart && gl.t <= gl.config.hdf5Stend) paraio.pstream(gl);
+	      if (
+		  (gl.config.hdf5_A_matrix_output == 1)
+		  //ToDo: (gl.config.hdf5_mass_current_output == 1) ...
+		  //ToDo: (gl.config.hdf5_spin_current_output == 1) ...		  		  
+		   && (gl.t >= gl.config.hdf5Ststart && gl.t <= gl.config.hdf5Stend)
+		 )
+		paraio.pstream(gl);
+	      else if (
+		       // insitu visualization block, no parallel hd5 stream
+                       (gl.config.hdf5_A_matrix_output != 1)
+		       && (
+                           (gl.config.do_gapA_clip == 1)
+			   || (gl.config.do_fed_clip == 1)
+			   || (gl.config.do_gapA_isosurface == 1)
+                          )
+                      )
+		paraio.pstream(gl);		
 	      //hila::out0 << "paraio.pstream() call is done " << std::endl;
 #endif	            
 	      meas_timer.stop();
-            }
+	   } // streaming block
 
-	    // Set gamma to 2nd value after certain momentum
-	    if (stat_counter >= (gl.config.gammaoffc)*steps) {gl.config.gamma = gl.config.gamma2;}
+	   
+            /* config.gamma update block */
+	    if (
+		(gl.config.TDependnetgamma == 1)
+		 && (stat_counter < (gl.config.gammaoffc)*steps)
+	       )
+	      // update gl.config.gamma if T-dependency of gamma is turned on
+	      {
+		gl.config.gamma = (gl.T.get_element(originpoints) < gl.MP.Tcp_mK(gl.config.Inip))
+		  ? gl.MP.gamma_td(gl.config.Inip, gl.T.get_element(originpoints))
+		  : gl.MP.gamma_td(gl.config.Inip, gl.MP.Tcp_mK(gl.config.Inip));
+	      }
+	    else if (stat_counter >= (gl.config.gammaoffc)*steps)
+      	      // Set gamma to 2nd value after certain momentum no matter what
+	      {gl.config.gamma = gl.config.gamma2;}
+	    /* config.gamma update block end here */
+	    
 	    //if (stat_counter == (gl.config.gammaoffc + 3)*steps) {gl.config.gamma = gl.config.gamma1;}
 	    if (stat_counter == (gl.config.BCchangec)*steps) {gl.config.boundaryConditions = gl.config.BCs2;}
-            ++stat_counter;
+
+	    ++stat_counter;
 
         } //gl.t > gl.config.Stats block	
 	
@@ -133,13 +166,13 @@ int main(int argc, char **argv) {
 	    ((gl.config.useTbath == 1)
 	     && (gl.t >= gl.config.Tbath_start)
 	     && (gl.config.evolveT == 0)
-	     && (gl.config.gamma == gl.config.gamma1)) // fixed T-thermal bath
+	     && (gl.config.gamma.abs() < gl.config.gamma2.abs())) // fixed T-thermal bath
 	    ||
 	    ((gl.config.useTbath == 1)
 	     && (gl.t >= gl.config.Tbath_start)
 	     && (gl.config.evolveT == 1)
 	     && (gl.t <= gl.config.tThermalizationWaiting)
-	     && (gl.config.gamma == gl.config.gamma1)) // eolved T-thermal bath run, but in fixed T thermalization	    
+	     && (gl.config.gamma.abs() < gl.config.gamma2.abs())) // eolved T-thermal bath run, but in fixed T thermalization	    
 	   )
 	  {
 	    gl.next_bath();
@@ -155,13 +188,13 @@ int main(int argc, char **argv) {
 		 && (gl.config.Tevolvetype == 2)
 		 && (gl.t > gl.config.tThermalizationWaiting)
 		 && (gl.config.withHfield != 1)
-		 && (gl.config.gamma == gl.config.gamma1)
+		 && (gl.config.gamma.abs() < gl.config.gamma2.abs())
                 )
 	  {
             gl.next_bath_UniT_quench();
-	    hila::out0 << "gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-		       << " next_bath_UniT_quench() call, T in site is " << gl.T.get_element(originpoints)
-		       << " Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
+	    hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
+		       << ", next_bath_UniT_quench() call, T in site is " << gl.T.get_element(originpoints)
+		       << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
 		       << std::endl;	    
 
 	  }
@@ -172,14 +205,14 @@ int main(int argc, char **argv) {
 		 && (gl.config.Tevolvetype == 2)
 		 && (gl.t > gl.config.tThermalizationWaiting)
 		 && (gl.config.withHfield == 1)
-		 && (gl.config.gamma == gl.config.gamma1)
+		 && (gl.config.gamma.abs() < gl.config.gamma2.abs())
                 )
 	  {
             gl.next_bath_UniT_quench_Hfield();
-	    hila::out0 << "gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-		       << " next_bath_UniT_quench_Hfield() call, T in site is " << gl.T.get_element(originpoints)
-	               << " |H| is " << norm(gl.H.get_element(originpoints))
-		       << " Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
+	    hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
+		       << ", next_bath_UniT_quench_Hfield() call, T in site is " << gl.T.get_element(originpoints)
+	               << ", |H| is " << norm(gl.H.get_element(originpoints))
+		       << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
 		       << std::endl;	    
 
 	  }
@@ -188,8 +221,8 @@ int main(int argc, char **argv) {
 	    // if gl.config.gamma != gl.config.gamma1, program is doing frozen structure
 	    gl.next();
 	    hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-		       << " next() call, T in site is " << gl.T.get_element(originpoints)
-		       << " Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
+		       << ", next() call, T in site is " << gl.T.get_element(originpoints)
+		       << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
 		       << std::endl;	    
 	    
 	  }
