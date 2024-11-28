d9f283bef3d72a5a04ca624e9c6957e3f270f8fe
timohyva <hyvarinentimo00@gmail.com>
2024-11-28 12:36:01 +0200

    add objects into matep_conf.mk; clearn off data set from matep/src/matep_utils.cpp

--- matep/matep_conf.mk
+++ matep/matep_conf.mk
@@ -8,7 +8,9 @@ vpath %.cpp matep/src
 APP_OPTS += -I $(DYGILA_DIR)/matep/inc
 
 # matep object, built by HILA pattern rules
-MATEP_OBJECTS = build/matep.o build/matep_utils.o
+MATEP_OBJECTS = build/matep.o \
+	        build/matep_utils.o \
+	        build/init_global_wrapper_mp.o
 
 .PHONY: matep
 matep: $(MATEP_OBJECTS)
