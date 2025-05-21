6a2685f6883c52c8f5e2154a3e831647fdf5a9a2
timohyva <hyvarinentimo00@gmail.com>
2025-05-21 20:42:27 +0300

    refine the Ascent container varibles

--- pario/inc/pario.hpp
+++ pario/inc/pario.hpp
@@ -7,7 +7,6 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
-//#include <math.h>
 #include <assert.h>
 
 #include "plumbing/hila.h"
@@ -75,36 +74,36 @@ private:
   
   Field<real_t> gapA;
   Field<real_t> feDensity;
-  Field<real_t> Temperature_field;
-  Field<real_t> phaseMarker_field;
     // Field<real_t> trA_re, trA_im;
   Field<real_t> u11, u12, u13, u21, u22, u23, u31, u32, u33;
   Field<real_t> v11, v12, v13, v21, v22, v23, v31, v32, v33;
     // Field<real_t> eigAv1, eigAv2, eigAv3;
   Field<real_t> jm1, jm2, jm3;
-  Field<real_t> phaseExpModulus, phaseExpAngle,/*acosphi, asinphi,*/ phaseExp2Re, phaseExp2Im;
+  Field<real_t> phaseExpModulus, phaseExpAngle/*acosphi, asinphi,*/
+                ,phaseExp2Re, phaseExp2Im;
   Field<real_t> js11, js21, js31,
                 js12, js22, js32,
                 js13, js23, js33;
    
-  std::vector<real_t> gapAOrdered;
-  std::vector<real_t> feDensityOrdered;
+  std::vector<real_t> gapAContainer;
+  std::vector<real_t> feDensityContainer;
   std::vector<real_t> Temperature;
   std::vector<real_t> phaseMarker;  
 
-  // std::vector<real_t> trA_reOrdered, trA_imOrdered;
-  std::vector<real_t> u11Ordered, u12Ordered, u13Ordered,
-                      u21Ordered, u22Ordered, u23Ordered,
-                      u31Ordered, u32Ordered, u33Ordered;
-  std::vector<real_t> v11Ordered, v12Ordered, v13Ordered,
-                      v21Ordered, v22Ordered, v23Ordered,
-                      v31Ordered, v32Ordered, v33Ordered;
-    // std::vector<real_t> eigAv1Ordered, eigAv2Ordered, eigAv3Ordered;
-  std::vector<real_t> jm1Ordered, jm2Ordered, jm3Ordered;
-  std::vector<real_t> phaseExpModulusO, phaseExpAngleO,/*acosphiO, asinphiO,*/ phaseExp2ReO, phaseExp2ImO;
-  std::vector<real_t> js11O, js21O, js31O,
-                      js12O, js22O, js32O,
-                      js13O, js23O, js33O;
+  // std::vector<real_t> trA_reContainer, trA_imContainer;
+  std::vector<real_t> u11Container, u12Container, u13Container,
+                      u21Container, u22Container, u23Container,
+                      u31Container, u32Container, u33Container;
+  std::vector<real_t> v11Container, v12Container, v13Container,
+                      v21Container, v22Container, v23Container,
+                      v31Container, v32Container, v33Container;
+    // std::vector<real_t> eigAv1Container, eigAv2Container, eigAv3Container;
+  std::vector<real_t> jm1Container, jm2Container, jm3Container;
+  std::vector<real_t> phaseExpModulusContanier, phaseExpAngleContanier /*acosphiContanier, asinphiContanier,*/
+                      ,phaseExp2ReContanier, phaseExp2ImContanier;
+  std::vector<real_t> js11Contanier, js21Contanier, js31Contanier,
+                      js12Contanier, js22Contanier, js32Contanier,
+                      js13Contanier, js23Contanier, js33Contanier;
   
   /*--------------------------------*/
   
