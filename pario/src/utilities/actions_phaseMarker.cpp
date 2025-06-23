f7efbb0b99e3fb0fed3c3e82b9b2b9ee92aec9dc
timohyva <hyvarinentimo00@gmail.com>
2025-06-24 01:26:17 +0300

    modify actions_AMatrix.cpp and actions_phaseMarker.cpp with e1 and e4 for extract options. modify allocate.cpp for all hdf5 stream switch to triger acqirements of extract hdf5 start and end stram time.

---
+++ pario/src/utilities/actions_phaseMarker.cpp
@@ -0,0 +1,35 @@
+#define USE_MPI 
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <assert.h>
+
+#include "plumbing/hila.h"
+#include "plumbing/fft.h"
+
+#include "glsol.hpp"
+#include "matep.hpp"
+#include "pario.hpp"
+
+#include "ascent.hpp"
+#include "conduit_blueprint.hpp"
+
+
+void parIO::defineActions_phaseMarker() {
+
+    // /* >>>>>>>>>>>>> extract hdf5 <<<<<<<<<<<<<< */
+
+     conduit::Node &add_act8 = actions.append();
+     add_act8["action"] = "add_extracts";
+
+     conduit::Node &extracts = add_act8["extracts"];
+     extracts["e4/type"] = "relay";
+     extracts["e4/params/path"] = "pio/dyGiLa-sim-pMarker";
+     extracts["e4/params/protocol"] = "blueprint/mesh/hdf5";
+    
+     extracts["e4/params/fields"].append().set("phaseMarker");
+    
+} // defineActions() call end here
+
