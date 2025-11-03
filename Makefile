b326f81a5fe46d4107d8562a1174a41006bfe57f
timohyva <hyvarinentimo00@gmail.com>
2025-11-03 09:20:01 +0200

    import orch:: func calls into main.cpp; add orch:: func build into Makefile

--- Makefile
+++ Makefile
@@ -8,8 +8,8 @@
 # Absolute UNIX path of library HILA
 # Absolute UNIX path of parallel io library Ascent
 ifeq ($(ARCH), lumi)
-HILA_DIR:= /projappl/project_462000465/insHILA-II
-ASCENT_DIR := /projappl/project_462000465/ascent/install/ascent-v0.9.0
+HILA_DIR:= /projappl/project_462000836/insHILA-v0.0.2
+ASCENT_DIR := /projappl/project_462000836/ascent-v0.9.3-mpi/install/ascent-v0.9.3
 #ASCENT_DIR := /projappl/project_462000465/ascent-0.9.3/scripts/build_ascent/install/ascent-develop
 else
   ifeq ($(ARCH), lumi-hip-CC)
@@ -34,7 +34,7 @@ endif
 
 # absolute UNIX path of dyGiLa folder
 ifeq ($(ARCH), lumi)
- DYGILA_DIR := /projappl/project_462000465/dyGiLa-blob
+ DYGILA_DIR := /projappl/project_462000836/dyGiLa-develop
 else
   ifeq ($(ARCH), lumi-hip-CC)
    DYGILA_DIR := /projappl/project_462000836/dyGiLa-develop
@@ -58,25 +58,22 @@ APP_OPTS := -DNDIM=3
 include $(HILA_DIR)/libraries/main.mk     \
         $(DYGILA_DIR)/glsol/glsol_conf.mk \
 	$(DYGILA_DIR)/matep/matep_conf.mk \
-        $(DYGILA_DIR)/pario/pario_conf.mk
+        $(DYGILA_DIR)/pario/pario_conf.mk \
+        $(DYGILA_DIR)/orch/orch_conf.mk
 
 # With multiple targets we want to use "make target", not "make build/target".
 # This is needed to carry the dependencies to build-subdir
 dyGiLa: build/dyGiLa ; @:
 
 # Now the linking step for each target executable
-build/dyGiLa: Makefile $(GLSOL_OBJECTS) $(PARIO_OBJECTS) $(MATEP_OBJECTS) \
-               build/main.o \
-               $(HILA_OBJECTS) $(HEADERS)
-	$(LD) -o $@ $(GLSOL_OBJECTS) $(PARIO_OBJECTS) $(MATEP_OBJECTS) \
-		build/main.o \
-		$(HILA_OBJECTS) \
-		$(LDFLAGS) $(LDLIBS)
+build/dyGiLa: Makefile $(ORCH_OBJECTS) $(GLSOL_OBJECTS) $(PARIO_OBJECTS) $(MATEP_OBJECTS) \
+                build/main.o \
+                $(HILA_OBJECTS) $(HEADERS)
+	$(LD) -o $@ $(ORCH_OBJECTS) $(GLSOL_OBJECTS) $(PARIO_OBJECTS) $(MATEP_OBJECTS) \
+                build/main.o $(HILA_OBJECTS) $(LDFLAGS) $(LDLIBS)
 
 # build/dyGiLa: Makefile $(GLSOL_OBJECTS) $(MATEP_OBJECTS) \
-#               build/main.o \
-#               $(HILA_OBJECTS) $(HEADERS)
+#                build/main.o \
+#                $(HILA_OBJECTS) $(HEADERS)
 # 	$(LD) -o $@ $(GLSOL_OBJECTS) $(MATEP_OBJECTS) \
-#               build/main.o \
-# 	      $(HILA_OBJECTS) \
-# 	      $(LDFLAGS) $(LDLIBS)
+#                build/main.o $(HILA_OBJECTS) $(LDFLAGS) $(LDLIBS)
--- main.cpp
+++ main.cpp
@@ -8,11 +8,12 @@
 #include <assert.h>
 
 #include "plumbing/hila.h"
-#include "plumbing/fft.h"
-#include "plumbing/globals.h" 
+//#include "plumbing/fft.h"
+//#include "plumbing/globals.h" 
 
 #include "glsol.hpp"
-#include "matep_namespace_utils.hpp" 
+//#include "matep_namespace_utils.hpp"
+#include "orch.hpp"
 
 #if defined USE_PARIO 
 #include "pario.hpp"
@@ -23,70 +24,35 @@ int main(int argc, char **argv) {
     glsol gl;      
        
     const std::vector<std::string> name_files = gl.allocate("sim_params.txt", argc, argv);
-
     // initialize blobal matep wrapper
-    matep::init_wrapper_mp();    
-       
+    matep::init_wrapper_mp();           
     // initialize Temperature field
     gl.initializeT();
-
     // initialize pressure field
     // gl.initializep();
-
     // initilize static H-field
     gl.initializeH();
-
     // initialize OP field
     gl.initialize();   
            
-    int stepspos;
-
     std::initializer_list<int> coordsList {0,0,0};
     const CoordinateVector originpoints(coordsList); 
 
-    // number of steps between printing stats
+    // number of steps between reduction streaming
     const unsigned int steps = (gl.config.tEnd - gl.config.tStats) 
                                /(gl.config.dt * gl.config.nOutputs); 
     
-
     // initial gamma parameter if gamma is fixed
     if (gl.config.TDependnetgamma == 0) { gl.config.gamma = gl.config.gamma1; } 
 
     // initial bounaryConstions
-    gl.config.boundaryConditions = gl.config.BCs1;               
-    
-    if (gl.config.positions == 1) {
-      stepspos = (gl.config.tEnd - gl.config.tStats) 
-                  /(gl.config.dt * gl.config.npositionout);
-      if (stepspos == 0)
-        stepspos = 1;
-    }
-
-    // measurement and stream counter
-    unsigned int stat_counter = 0;
-
-    if (hila::myrank() == 0) { gl.fstreams_open(name_files); }
-
+    gl.config.boundaryConditions = gl.config.BCs1;
+    if (hila::myrank() == 0) { gl.fstreams_open(name_files); }    
+        
 #if defined USE_PARIO
-    parIO paraio;
-    
+    parIO paraio;    
     //xml files for MetaData.    
-    if (
-        (gl.config.hdf5_A_matrix_output        == 1)
-	// || (gl.config.hdf5_trA_output          == 1)
-	// || (gl.config.hdf5_eigvA_output        == 1)
-	|| (gl.config.hdf5_mass_current_output == 1)
-	|| (gl.config.hdf5_spin_current_output == 1)
-	|| (gl.config.hdf5_pMarker_output == 1)
-       )
-    {
-      if (gl.config.hdf5_A_matrix_output == 1) paraio.xml_Amatrix(gl);
-      if (gl.config.hdf5_pMarker_output == 1) paraio.xml_pMarker(gl);
-      if (gl.config.hdf5_mass_current_output == 1) paraio.xml_massCurrent(gl);
-      if (gl.config.hdf5_spin_current_output == 1) paraio.xml_spinCurrent(gl);      
-     //hila::synchronize();
-    }
-    
+    orch::writeHDF5_xmls(gl, paraio);    
     paraio.init(gl);
 
     if (hila::myrank() == 0) paraio.xdmf(gl);              
@@ -97,239 +63,44 @@ int main(int argc, char **argv) {
     /* Dynamic simulation starts after below.                            */
     /*                                                                   */
     /* on gpu the simulation timer is fake, because there's no sync here.*/  
-    /* BUt we want to avoid unnecessary sync anyway.                     */
+    /* But we want to avoid unnecessary sync anyway.                     */
     /*-------------------------------------------------------------------*/      
     static hila::timer run_timer("Simulation time"), meas_timer("Measurements");
     run_timer.start();
-    
+    // measurement and stream counter
+    unsigned int stat_counter = 0;
+        
     while (gl.t < gl.config.tEnd) {
       
         if (gl.t > gl.config.tStats) {
 	  
 	   if (stat_counter % steps == 0) {
-
 	      meas_timer.start();
-	      gl.write_energies();
-	      gl.phaseCounting();
-	      hila::out0 << "write_energies(), phaseCounting() call is done "
-			 << std::endl;
-
-#if defined USE_PARIO
-	      if (
-		  ((gl.config.hdf5_A_matrix_output == 1)
-		   || (gl.config.hdf5_pMarker_output == 1)
-		   || (gl.config.hdf5_mass_current_output == 1)
-		   || (gl.config.hdf5_spin_current_output == 1))
-		   && (gl.t >= gl.config.hdf5Ststart && gl.t <= gl.config.hdf5Stend)
-		 )
-		paraio.pstream(gl, stat_counter);
-	      else if (
-		       // insitu visualization block, no parallel hd5 stream
-                       (gl.config.hdf5_A_matrix_output != 1)
-		       && (gl.config.hdf5_pMarker_output != 1)
-		       && (gl.config.hdf5_mass_current_output != 1)
-		       && (gl.config.hdf5_spin_current_output != 1)		       
-		       && (
-                           (gl.config.do_gapA_clip == 1)
-			   || (gl.config.do_gapA_slice == 1)
-			   || (gl.config.do_fed_clip == 1)
-			   || (gl.config.do_Temperature_clip == 1)
-			   || (gl.config.do_Temperature_slice == 1)
-			   || (gl.config.do_Temperature_isosurface == 1)			   
-			   || (gl.config.do_gapA_isosurface == 1)
-			   || (gl.config.do_phaseMarker_slice == 1)
-			   || (gl.config.do_phaseMarker_isosurface == 1)
-			   || (gl.config.do_phaseMarker_fieldclip == 1)
-			   || (gl.config.do_phaseMarker_fieldclip_Bphase == 1)
-			   || (gl.config.do_phaseMarker_fieldclip_Aphase == 1)
-                          )
-                      )
-		paraio.pstream(gl, stat_counter);		
-	      hila::out0 << "paraio.pstream() call is done " << std::endl;
-#endif	            
+	      orch::pStreaming(gl, paraio, stat_counter);
 	      meas_timer.stop();
 	   } // streaming block
 
+	   // gamma handling
+	   orch::gammaEvolve(gl, stat_counter, steps, originpoints);
 	   
-            /* config.gamma update block */
-	    if (
-		(gl.config.TDependnetgamma == 1)
-		&& (gl.config.initialConditionT != 2)
-		&& (stat_counter < (gl.config.gammaoffc)*steps)
-	       )
-	      // update gl.config.gamma if T-dependency of gamma is turned on
-	      {
-		   gl.config.gamma = (gl.T.get_element(originpoints) < gl.MP.Tcp_mK(gl.config.Inip))
-		     ? gl.MP.gamma_td(gl.config.Inip, gl.T.get_element(originpoints), gl.phaseMarker.get_element(originpoints))
-		     : gl.MP.gamma_td(gl.config.Inip, gl.MP.Tcp_mK(gl.config.Inip), gl.phaseMarker.get_element(originpoints));
-		
-	      }
-	    else if (
-		     (gl.config.initialConditionT != 2)
-		     && (stat_counter >= (gl.config.gammaoffc)*steps)
-		    )
-      	      // Set gamma to 2nd value after certain momentum for non-blob profile
-	      {gl.config.gamma = gl.config.gamma2;}
-            else if (
-                     (gl.config.initialConditionT == 2)
-		     && (stat_counter >= (gl.config.gammaoffc)*steps)
-                    )
-      	      // Set gamma to 2nd value after certain momentum for blob profile
-	      {gl.config.gamma = gl.config.gamma2;}	      	    
-	    /* config.gamma update block end here */
-	    
-	    if (stat_counter == (gl.config.BCchangec)*steps) {gl.config.boundaryConditions = gl.config.BCs2;}
+	   if (stat_counter == (gl.config.BCchangec)*steps)
+	     { gl.config.boundaryConditions = gl.config.BCs2; }
 
-	    ++stat_counter;
+	   ++stat_counter;
 
         } //gl.t > gl.config.Stats block
 
-        gl.phaseMarking();        
-        hila::out0 << "gl.t is " << gl.t << ", phaseMarking() call is done. " << std::endl;
-	
-
-	/*******************************************************************/
-	/* the following if else blocks are different system synamic updates  
-         *  all next_xxx() functions call happen here.
-         */
-	/*******************************************************************/	
-	
-	if (
-	    (gl.config.initialConditionT == 2)
-	    && (gl.config.evolveT == 1)
-	   )
-	  {
-	    if (stat_counter < (gl.config.gammaoffc)*steps)
-	      {
-	       //hila::out0 << "just before call next-blob() " << std::endl;
-	       if ( gl.config.use_AdGRz_surfaces == 1 )
-		 // Quasi 2D Cylinderial blob with AdGR boundary for high-T
-		 { /*gl.next_bath_Quasi2Dhotblob_quench_AdGR_Hfield();*/ }
-	       else
-		 // 3D blob without boundary surface effect
-	         { gl.next_bath_hotblob_quench_Hfield(); }
-	       
-	       if (stat_counter % steps == 0)
-	         {// squeze IO a littble bit
-	          hila::out0 << "gl.t is " << gl.t 
-		             << ", next_bath_hotblob_quench_Hfield() call " 
-		             << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		             << ", Ttdb0 is " << gl.config.Ttdb0	      
-	                     << std::endl;
-	         }
-
-	      } // blob evolution block
-	    else if (stat_counter >= (gl.config.gammaoffc)*steps)
-	      {
-	       ++gl.extinguish_t; //estinguish time count, in step of dt
-	       gl.next_bath_hotblob_quench_Hfield_confCatch();
-	       if (stat_counter % steps == 0)
-	         {// squeze IO a littble bit
-	          hila::out0 << "gl.t is " << gl.t 
-		             << ", next_bath_hotblob_quench_Hfield_confCatch() call " 
-		             << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		             << ", Ttdb0 is " << gl.config.Ttdb0	      
-	                     << std::endl;
-	         }
-
-	      } // conf catch block 
-	      
-	  } // heterogenous quench, hot blob T-profile 
-	else
-	  { // homogenous quench block starts from here
-	if (
-	    ((gl.config.useTbath == 1)
-	     && (gl.t >= gl.config.Tbath_start)
-	     && (gl.config.evolveT == 0)
-	     && (gl.config.gamma.abs() < gl.config.gamma2.abs())) // fixed T-thermal bath
-	    ||
-	    ((gl.config.useTbath == 1)
-	     && (gl.t >= gl.config.Tbath_start)
-	     && (gl.config.evolveT == 1)
-	     && (gl.t <= gl.config.tThermalizationWaiting)
-	     && (gl.config.gamma.abs() < gl.config.gamma2.abs())) // eolved T-thermal bath run, but in fixed T thermalization w/o AdGR boundary
-	   )
-	  {
-	    if (gl.config.use_AdGRz_surfaces != 1)
-	      {
-	       gl.next_bath();
-	       hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-		          << ", next_bath() call, T in site is " << gl.T.get_element(originpoints)
-		          << " Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		          << std::endl;	    
-
-	      }
-	    else if (gl.config.use_AdGRz_surfaces == 1)
-	      {
-	       gl.next_AdGRz_bath();
-	       hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-		          << ", next_AdGRz_bath() call, T in site is " << gl.T.get_element(originpoints)
-		          << " Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		          << std::endl;	    
-	      }
-	  }
-	else if (
-	         (gl.config.useTbath == 1)
-	         && (gl.t >= gl.config.Tbath_start)
-		 && (gl.config.evolveT == 1)
-		 && (gl.config.Tevolvetype == 2)
-		 && (gl.t > gl.config.tThermalizationWaiting)
-		 && (gl.config.withHfield != 1)
-		 && (gl.config.gamma.abs() < gl.config.gamma2.abs())
-                )
-	  {
-            gl.next_bath_UniT_quench();
-	    hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-		       << ", next_bath_UniT_quench() call, T in site is " << gl.T.get_element(originpoints)
-		       << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		       << std::endl;	    
-
-	  }
-	else if (
-	         (gl.config.useTbath == 1)
-	         && (gl.t >= gl.config.Tbath_start)
-		 && (gl.config.evolveT == 1)
-		 && (gl.config.Tevolvetype == 2)
-		 && (gl.t > gl.config.tThermalizationWaiting)
-		 && (gl.config.withHfield == 1)
-		 && (gl.config.gamma.abs() < gl.config.gamma2.abs())
-                )
-	  {
-	    if (gl.config.use_AdGRz_surfaces != 1)
-	      {
-                gl.next_bath_UniT_quench_Hfield();
-         	hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-	      	           << ", next_bath_UniT_quench_Hfield() call, T in site is " << gl.T.get_element(originpoints)
-	                   << ", |H| is " << norm(gl.H.get_element(originpoints))
-		           << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		           << std::endl;	    
-	      }
-	    else if (gl.config.use_AdGRz_surfaces == 1)
-	      {
-                gl.next_bath_UniT_quench_AdGRz_Hfield();
-         	hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-	      	           << ", next_bath_UniT_quench_AdGR_Hfield() call, T in site is " << gl.T.get_element(originpoints)
-	                   << ", |H| is " << norm(gl.H.get_element(originpoints))
-		           << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		           << std::endl;	    
-
-	      }
-
-	  }
-	else
-	  {
-	    // if gl.config.gamma != gl.config.gamma1, program is doing frozen structure
-	    gl.next();
-	    hila::out0 << " gl.t is " << gl.t << ", gl.config.gamma is " << gl.config.gamma
-		       << ", next() call, T in site is " << gl.T.get_element(originpoints)
-		       << ", Tc is " << gl.MP.Tcp_mK(gl.config.Inip)
-		       << std::endl;	    
-	    
+	if (gl.config.TDependnetgamma == true) {
+	    // do every-dt phase-Marking when gamma is T-dependent
+            gl.phaseMarking();        
+            hila::out0 << "gl.t is " << gl.t
+		       << ", phaseMarking() call is done. "
+		       << std::endl;
 	  }
 
-	  } // homogenous quench block
-	    
-		
+        // t-evolve call
+        orch::nextBlocks(gl, stat_counter, steps, originpoints);
+         	    		
     } // gl.t evolves while loop ends here
     run_timer.stop();
 
