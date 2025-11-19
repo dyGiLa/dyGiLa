88d7113023f5ad827fb98e53765f06a94a575b64
timohyva <hyvarinentimo00@gmail.com>
2025-11-19 17:11:35 +0200

    add newest ROCm lumi batch scripts

--- batch_scripts/submit-ROCm-lumi-test.sh
+++ batch_scripts/submit-ROCm-lumi-test.sh
@@ -1,19 +1,14 @@
 #!/bin/bash -l
-#SBATCH --job-name=dyGILaG   # Job name
+#SBATCH --job-name=dyGiLaG   # Job name
 #SBATCH --output=dyGiLa-ROCm.o%j # Name of stdout output file
 #SBATCH --error=dyGiLa-ROCm.e%j  # Name of stderr error file
-#SBATCH --partition=dev-g  # partition name
+#SBATCH --partition=dev-g # partition name
 #SBATCH --nodes=2               # Total number of nodes 
 #SBATCH --ntasks-per-node=8     # 8 MPI ranks per node, 16 total (2x8)
 #SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
 #SBATCH --exclusive
 #SBATCH --time=0-00:30:00       # Run time (d-hh:mm:ss)
-#SBATCH --account=project_462000836  # Project for billing
-
-# module --force purge && module --force unload LUMI
-# module load LUMI/24.03 partition/G cpeCray/24.03 buildtools/24.03 rocm/6.0.3
-
-# don't set SBATCH --exclusive if you use select_gpu
+#SBATCH --account=project_462000960  # Project for billing
 
 export MPICH_GPU_SUPPORT_ENABLED=1
 
@@ -26,7 +21,11 @@ dpioCurrent="./pio_Current"
 
 if [ ! -d $dstats ] && [ ! -d $dxmls ] && [ ! -d $dxdmf ] && [ ! -d $dinsitu ] && [ ! -d $dpio ] && [ ! -d $dpioCurrent ]; then
     mkdir stats rank_xmls xdmf insitu pio pio_Current;
+    cd insitu && mkdir gapA-clip1 gapA-clip2 gapA-slice1 gapA-iso feDensity-clip_Camp Temeperature-clip Temperature-slice Temperature-iso pMarker-Slice pMarker-iso pMarker-fieldclip pMarker-isoVolume-Bphase pMarker-fieldclip-Aphase;
+    cd ..
 fi    
 
-srun /projappl/project_462000836/dyGiLa-IO/build/dyGiLa -i sim_config_dyGiLa-Langevin-blob-quench-Hfield-30mT.txt
-#rm -rf ./select_gpu
+# srun --cpu-bind=${CPU_BIND} ./select_gpu /projappl/project_462000960/dyGiLa-develop/build/dyGiLa -i sim_config_dyGiLa-Langevin-blob-quench-Hfield-30mT.txt
+srun /projappl/project_462000960/dyGiLa-develop-lite/build/dyGiLa -i sim_config_dyGiLa-Langevin-blob-quench-Hfield-30mT.txt
+
+rm -rf ./select_gpu
--- batch_scripts/submit-ROCm-lumi.sh
+++ batch_scripts/submit-ROCm-lumi.sh
@@ -3,14 +3,11 @@
 #SBATCH --output=dyGiLa-ROCm.o%j # Name of stdout output file
 #SBATCH --error=dyGiLa-ROCm.e%j  # Name of stderr error file
 #SBATCH --partition=standard-g  # partition name
-#SBATCH --nodes=4               # Total number of nodes 
+#SBATCH --nodes=6               # Total number of nodes 
 #SBATCH --ntasks-per-node=8     # 8 MPI ranks per node, 16 total (2x8)
 #SBATCH --gpus-per-node=8       # Allocate one gpu per MPI rank
 #SBATCH --time=2-00:00:00       # Run time (d-hh:mm:ss)
-#SBATCH --account=project_462000836  # Project for billing
-
-module --force purge && module --force unload LUMI
-module load LUMI/24.03 partition/G cpeCray/24.03 buildtools/24.03 rocm/6.0.3
+#SBATCH --account=project_462000960  # Project for billing
 
 cat << EOF > select_gpu
 #!/bin/bash
@@ -34,8 +31,10 @@ dpioCurrent="./pio_Current"
 
 if [ ! -d $dstats ] && [ ! -d $dxmls ] && [ ! -d $dxdmf ] && [ ! -d $dinsitu ] && [ ! -d $dpio ] && [ ! -d $dpioCurrent ]; then
     mkdir stats rank_xmls xdmf insitu pio pio_Current;
+    cd insitu && mkdir gapA-clip1 gapA-clip2 gapA-slice1 gapA-iso feDensity-clip_Camp Temeperature-clip Temperature-slice Temperature-iso pMarker-Slice pMarker-iso pMarker-fieldclip pMarker-isoVolume-Bphase pMarker-fieldclip-Aphase;
+    cd ..
 fi    
 
-srun --cpu-bind=${CPU_BIND} ./select_gpu /projappl/project_462000836/dyGiLa-IO/build/dyGiLa -i sim_config_dyGiLa-Langevin-blob-quench-Hfield-30mT.txt
+srun --cpu-bind=${CPU_BIND} ./select_gpu /projappl/project_462000960/dyGiLa-develop/build/dyGiLa -i sim_config_dyGiLa-Langevin-blob-quench-Hfield-30mT.txt
 
 rm -rf ./select_gpu
