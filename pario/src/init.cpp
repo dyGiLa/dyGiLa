c5854a4b24a22421349ec55d35f82179dae77816
timohyva <hyvarinentimo00@gmail.com>
2026-01-04 16:35:16 +0200

    adapting utils/pioInit/ funcs

--- pario/src/init.cpp
+++ pario/src/init.cpp
@@ -1,16 +1,16 @@
-#define USE_BOTHSIDE_GHOSTS
-#define USE_ADGRZ
+// #define USE_BOTHSIDE_GHOSTS
+// #define USE_ADGRZ
 #define USE_MPI 
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <string>
-#include <assert.h>
+// #include <sstream>
+// #include <iostream>
+// #include <iomanip>
+// #include <fstream>
+// #include <string>
+// #include <assert.h>
 
 #include "plumbing/hila.h"
 //#include "plumbing/fft.h"
-#include "plumbing/memalloc.h"
+//#include "plumbing/memalloc.h"
 
 #include "glsol.hpp"
 //#include "matep.hpp"
@@ -22,104 +22,21 @@
 void parIO::init(glsol &sol) {
 
     latticeVolumeWithGhost =
-        (lattice.mynode.size[0] + 2) * (lattice.mynode.size[1] + 2) * (lattice.mynode.size[2] + 2);
-    
+        (lattice.mynode.size[0] + 2) * (lattice.mynode.size[1] + 2) * (lattice.mynode.size[2] + 2);  
     latticeVolume =
         (lattice.mynode.size[0]) * (lattice.mynode.size[1]) * (lattice.mynode.size[2]);
 
-    gapAContainer.reserve(latticeVolumeWithGhost);
+    /*********************************/
+    /*    container reserve calls    */
+    /*********************************/       
+    containerReserve_gapAFETemPMarker(sol);
+    if (sol.config.hdf5_mass_current_output == 1){ containerReserve_massCurrent(); }
+    if (sol.config.hdf5_spin_current_output == 1) { containerReserve_spinCurrent(); }
+    if (sol.config.hdf5_A_matrix_output == 1) { containerReserve_Amatrix(); }
+
+    // ghost handling
+    ghostMask(sol);
     
-    if (sol.config.pario_compute_feDensity == 1) { feDensityContainer.reserve(latticeVolumeWithGhost); }
-    
-    Temperature.reserve(latticeVolumeWithGhost);
-    phaseMarker.reserve(latticeVolumeWithGhost);
-
-    if (sol.config.hdf5_mass_current_output == 1){
-      jm1Container.reserve(latticeVolumeWithGhost);
-      jm2Container.reserve(latticeVolumeWithGhost);
-      jm3Container.reserve(latticeVolumeWithGhost);
-
-      phaseExpModulusContainer.reserve(latticeVolumeWithGhost);
-      phaseExpAngleContainer.reserve(latticeVolumeWithGhost);
-      phaseExp2ReContainer.reserve(latticeVolumeWithGhost);
-      phaseExp2ImContainer.reserve(latticeVolumeWithGhost);                  
-    }
-
-    if (sol.config.hdf5_spin_current_output == 1){
-      js11Container.reserve(latticeVolumeWithGhost);
-      js21Container.reserve(latticeVolumeWithGhost);
-      js31Container.reserve(latticeVolumeWithGhost);
-
-      js12Container.reserve(latticeVolumeWithGhost);
-      js22Container.reserve(latticeVolumeWithGhost);
-      js32Container.reserve(latticeVolumeWithGhost);
-
-      js13Container.reserve(latticeVolumeWithGhost);
-      js23Container.reserve(latticeVolumeWithGhost);
-      js33Container.reserve(latticeVolumeWithGhost);      
-    }
-
-    if (sol.config.hdf5_A_matrix_output == 1){
-     u11Container.reserve(latticeVolumeWithGhost); v11Container.reserve(latticeVolumeWithGhost);
-     u12Container.reserve(latticeVolumeWithGhost); v12Container.reserve(latticeVolumeWithGhost);
-     u13Container.reserve(latticeVolumeWithGhost); v13Container.reserve(latticeVolumeWithGhost);
-     u21Container.reserve(latticeVolumeWithGhost); v21Container.reserve(latticeVolumeWithGhost);
-     u22Container.reserve(latticeVolumeWithGhost); v22Container.reserve(latticeVolumeWithGhost);
-     u23Container.reserve(latticeVolumeWithGhost); v23Container.reserve(latticeVolumeWithGhost);
-     u31Container.reserve(latticeVolumeWithGhost); v31Container.reserve(latticeVolumeWithGhost);
-     u32Container.reserve(latticeVolumeWithGhost); v32Container.reserve(latticeVolumeWithGhost);
-     u33Container.reserve(latticeVolumeWithGhost); v33Container.reserve(latticeVolumeWithGhost);    
-    }
-     
-    // cell data (Npts - 1 cells) in each directions
-    auto ghostNX = lattice.mynode.size[0] + 2 - 1;
-    auto ghostNY = lattice.mynode.size[1] + 2 - 1;
-    auto ghostNZ = lattice.mynode.size[2] + 2 - 1;
-
-#ifdef USE_ADGRZ
-    //auto mynodeExtentZ = lattice.mynode.size[2]; 
-    auto mynodeMinZcoord = lattice.mynode.min[2];
-    auto mynodeMaxZcoord = lattice.mynode.min[2] + (lattice.mynode.size[2]-1 ); // z-roof coordinat index: min+(size-1) 
-
-    // hila::out << "mynodeMinZcoord is " << mynodeMinZcoord
-    // 	      << ", mynodeMaxZcoord is " << mynodeMaxZcoord
-    // 	      << std::endl;
-#endif    
-    
-    ghostVolume = ghostNX * ghostNY * ghostNZ;
-    ghostCellsMask = (unsigned char *)memalloc(ghostVolume * sizeof(unsigned char));
-
-    long long counter = 0;
-    unsigned char Mask = 0;
-
-    // i,j,k = 0 left-most halos
-    for (auto k = 0; k < ghostNZ; k++) {
-        for (auto j = 0; j < ghostNY; j++) {
-            for (auto i = 0; i < ghostNX; i++) {
-#if !defined(USE_ADGRZ) && defined(USE_BOTHSIDE_GHOSTS)
-	      bool kGhostFlag = (k == 0) || (k == (ghostNZ -1));
-              bool jGhostFlag = (j == 0) || (j == (ghostNY -1));
-              bool iGhostFlag = (i == 0) || (i == (ghostNX -1));	      
-#elif defined(USE_ADGRZ) && defined(USE_BOTHSIDE_GHOSTS)
-              bool kGhostFlag = ((k == 0) && !(mynodeMinZcoord == 0))
-		                || ((k == (ghostNZ -1)) && !(mynodeMaxZcoord == (sol.config.lz-1)));
-              bool jGhostFlag = (j == 0) || (j == (ghostNY -1));
-              bool iGhostFlag = (i == 0) || (i == (ghostNX -1));	      	      
-#elif !defined(USE_ADGRZ) && !defined(USE_BOTHSIDE_GHOSTS)
-	      bool kGhostFlag = (k == 0);
-              bool jGhostFlag = (j == 0);
-              bool iGhostFlag = (i == 0);	      	      
-#elif defined(USE_ADGRZ) && !defined(USE_BOTHSIDE_GHOSTS)
-              bool kGhostFlag = ((k == 0) && !(mynodeMinZcoord == 0));
-              bool jGhostFlag = (j == 0);
-              bool iGhostFlag = (i == 0);	      	      	      
-#endif		
-              Mask = static_cast<unsigned char>(iGhostFlag || jGhostFlag || kGhostFlag);
-              ghostCellsMask[counter++] = Mask;
-            }
-        }
-    }
-
     /*********************************/
     /*    all describeMesh calls     */
     /*********************************/    
