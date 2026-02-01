7ccf5dd929c4199581025c84015b331a89de22c7
timohyva <hyvarinentimo00@gmail.com>
2026-02-02 00:36:34 +0200

    clean off

--- pario/src/utilities/mesh_gapA_FEDensity.cpp
+++ pario/src/utilities/mesh_gapA_FEDensity.cpp
@@ -1,16 +1,16 @@
 #define USE_MPI 
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
+// #include <sstream>
+// #include <iostream>
+// #include <iomanip>
+// #include <fstream>
 #include <string>
-#include <assert.h>
+//#include <assert.h>
 
-#include "plumbing/hila.h"
-#include "plumbing/fft.h"
+//#include "plumbing/hila.h"
+//#include "plumbing/fft.h"
 
 #include "glsol.hpp"
-#include "matep.hpp"
+//#include "matep.hpp"
 #include "pario.hpp"
 
 #include "ascent.hpp"
--- pario/src/utilities/mesh_insitu_Temperature.cpp
+++ pario/src/utilities/mesh_insitu_Temperature.cpp
@@ -1,17 +1,17 @@
 #define USE_MPI 
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <string>
+// #include <sstream>
+// #include <iostream>
+// #include <iomanip>
+// #include <fstream>
+// #include <string>
 
-#include <assert.h>
+// #include <assert.h>
 
-#include "plumbing/hila.h"
-#include "plumbing/fft.h"
+// #include "plumbing/hila.h"
+// #include "plumbing/fft.h"
 
-#include "glsol.hpp"
-#include "matep.hpp"
+// #include "glsol.hpp"
+// #include "matep.hpp"
 #include "pario.hpp"
 
 #include "ascent.hpp"
