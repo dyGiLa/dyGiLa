d8fdf5b6c5c6f2493795abc545d8324a23144160
timohyva <hyvarinentimo00@gmail.com>
2026-04-15 02:50:45 +0300

    deploy clip extract over dyGiLa

--- pario/src/init.cpp
+++ pario/src/init.cpp
@@ -34,7 +34,12 @@ void parIO::init(glsol &sol) {
     if (sol.config.hdf5_spin_current_output == 1) { containerReserve_spinCurrent(); }
     if (sol.config.hdf5_A_matrix_output == 1) { containerReserve_Amatrix(); }
     if (sol.config.hdf5_lVector_output == 1) { containerReserve_lVec(); }
-    if ((sol.config.hdf5_GradPhiVector_output == 1) || (sol.config.hdf5_GradPhiVector_exaslice_output == 1)) { containerReserve_GradientPhiVec(); }        
+    if (
+	(sol.config.hdf5_GradPhiVector_output == 1)
+	|| (sol.config.hdf5_GradPhiVector_exaslice_output == 1)
+	|| (sol.config.hdf5_GradPhiVector_clip_output == 1)	
+       )
+      { containerReserve_GradientPhiVec(); }        
 
     // ghost handling
     ghostMask(sol);
@@ -55,7 +60,11 @@ void parIO::init(glsol &sol) {
        if (sol.config.hdf5_spin_current_output == 1) {describeMesh_spinCurrent();}
        if (sol.config.hdf5_A_matrix_output == 1) {describeMesh_AMatrix();}
        if (sol.config.hdf5_lVector_output == 1) {describeMesh_lVec();}
-       if ((sol.config.hdf5_GradPhiVector_output == 1) || (sol.config.hdf5_GradPhiVector_exaslice_output == 1)) { describeMesh_GradientPhiVec(); }	               
+       if (
+	   (sol.config.hdf5_GradPhiVector_output == 1)
+	   || (sol.config.hdf5_GradPhiVector_exaslice_output == 1)
+	   || (sol.config.hdf5_GradPhiVector_clip_output == 1)		   
+	  ) { describeMesh_GradientPhiVec(); }	               
       }
 
     describeMesh_addGhost_verify();
@@ -81,7 +90,8 @@ void parIO::init(glsol &sol) {
       {
        if (sol.config.hdf5_lVector_output == 1) {defineActions_lVec();}
        if (sol.config.hdf5_GradPhiVector_output == 1) {defineActions_GradientPhiVec();}
-       if (sol.config.hdf5_GradPhiVector_exaslice_output == 1) {defineActions_GradientPhiVec_exaslice(sol);}		              
+       if (sol.config.hdf5_GradPhiVector_exaslice_output == 1) {defineActions_GradientPhiVec_exaslice(sol);}
+       if (sol.config.hdf5_GradPhiVector_clip_output == 1) {defineActions_GradientPhiVec_clip(sol);}		                     
        if (sol.config.hdf5_mass_current_output == 1) {defineActions_massCurrent(sol);}
        if (sol.config.hdf5_spin_current_output == 1) {defineActions_spinCurrent(sol);}
        if (sol.config.hdf5_A_matrix_output == 1) {defineActions_AMatrix(sol);}
